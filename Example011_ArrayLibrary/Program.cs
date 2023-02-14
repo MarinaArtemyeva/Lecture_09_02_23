03eb38af629c0dc4326139f4138f3790d78c9993
Marina Artemyeva <martemyeva@yahoo.com>
2023-02-14 11:45:25 +0600

    Create Example011_ArrayLibrary

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -2,6 +2,7 @@
 {
     int length = collection.Length;
     int index = 0;
+
     while(index < length)
     {
         collection[index] = new Random().Next(1, 10);
@@ -13,6 +14,7 @@ void PrintArray(int[] col)
 {
     int count = col.Length;
     int position = 0;
+    
     while (position < count)
     {
         Console.WriteLine(col[position]);
