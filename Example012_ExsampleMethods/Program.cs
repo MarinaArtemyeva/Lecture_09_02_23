8c22b1a35cdda5823056a17e1b9585e3e4a5f1a9
Marina Artemyeva <martemyeva@yahoo.com>
2023-02-20 19:22:34 +0600

    Add code Example0012_ExsampleMethods

--- Example012_ExsampleMethods/Program.cs
+++ Example012_ExsampleMethods/Program.cs
@@ -42,5 +42,19 @@ string Method4(int count, string c)
     }
     return result;
 }
-string res = Method4(10, "asdf");
-Console.WriteLine(res);
+//string res = Method4(10, "asdf");
+//Console.WriteLine(res);
+
+//For цикл:
+
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+//string res = Method4(10, text); ???
+//Console.WriteLine(res); ???
