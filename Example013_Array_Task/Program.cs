8d99ead4a7e2be3d1056d82bc2ca7f82290ac2f3
Marina Artemyeva <martemyeva@yahoo.com>
2023-02-20 19:39:10 +0600

    Add code in Example013_Array_Task

---
+++ Example013_Array_Task/Program.cs
@@ -0,0 +1,34 @@
+﻿//Упорядочить массив 
+// 6 8 3 2 1 4 5 7
+//1. Найти позицию минимлаьного элемента в неотсортированной части массива;
+//2. Произвести обмен этого значения со значением первой неотсортированной позиции;
+//3. Повторять пока есть не отсортированные элементы.
+
+int [] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i =0; i < count; i++)
+    Console.WriteLine($"{array[i]} ");
+    Console.WriteLine();
+}
+PrintArray(arr);
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+    {
+        if (array[j] < array[minPosition])
+        {
+            minPosition = j;
+        }
+    }
+int temprorary = array[i];
+array[i] = array[minPosition];
+array[minPosition] = temprorary;
+}
+}
